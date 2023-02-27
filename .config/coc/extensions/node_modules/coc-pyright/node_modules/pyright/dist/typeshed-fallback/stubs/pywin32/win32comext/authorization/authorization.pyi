180f6c5f6750a5eae1dbb8fa31d1bbc52a631142
milktjj <milktjj@outlook.com>
2023-02-27 12:22:08 +0800

    remove

--- .config/coc/extensions/node_modules/coc-pyright/node_modules/pyright/dist/typeshed-fallback/stubs/pywin32/win32comext/authorization/authorization.pyi
+++
@@ -1,6 +0,0 @@
-import _win32typing
-from pywintypes import IID as IID
-
-def EditSecurity(*args, **kwargs): ...  # incomplete
-
-IID_ISecurityInformation: _win32typing.PyIID
--- .config/test.lua
+++
@@ -1,6 +0,0 @@
-for i = 100, 1, -1 do
-    io.write(i)
-    io.write("\n")
-end
-
-Cmd = (":%s<CR>"):format("Ctrl")
